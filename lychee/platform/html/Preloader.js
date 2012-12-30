4daf856cf2c8b177d110ba5fb40d1327988c9f10
Christoph Martens <christoph@martens.ms>
2012-12-30 02:42:03 +0100

    lychee.Preloader (html platform): New implementation supports multiple instances that dont collide in their fired events. Preloader is now theoretically movable to core via using a plugin for _load().

--- lychee/platform/html/Preloader.js
+++ lychee/platform/html/Preloader.js
@@ -2,18 +2,120 @@
 // Preloader is platform specific and required for lychee.Builder
 (function(lychee, global) {
 
-	var _loading = {},
-		_assets = {},
-		_data = {},
-		_alreadyFired = {},
-		_globalIntervalId = null;
+	var _instances = [];
+	var _cache = {};
 
-	var Class = function(settings) {
 
-		this.settings = lychee.extend({}, this.defaults, settings);
+	var _globalIntervalId = null;
+	var _globalInterval   = function() {
 
-		this.__events = {};
-		this.__lastLoadStart = null;
+		var timedOutInstances = 0;
+
+		for (var i = 0, l = _instances.length; i < l; i++) {
+
+			var instance = _instances[i];
+			var isReady  = true;
+
+			for (var url in instance.__pending) {
+				if (
+					instance.__pending[url] === true
+					|| _cache[url] === undefined
+				) {
+					isReady = false;
+				}
+			}
+
+
+			var timedOut = false;
+			if (instance.__clock !== null) {
+				timedOut = Date.now() >= instance.__clock + instance.__timeout;
+			}
+
+
+			if (isReady === true || timedOut === true) {
+
+				var errors = {};
+				var ready  = {};
+				var map    = {};
+
+				for (var url in instance.__pending) {
+
+					if (instance.__fired[url] === undefined) {
+
+						if (instance.__pending[url] === false) {
+
+							ready[url] = _cache[url] || null;
+							map[url] = instance.__map[url] || null;
+
+						} else {
+							errors[url] = null;
+						}
+
+						instance.__fired[url] = true;
+
+					}
+
+				}
+
+
+				if (Object.keys(errors).length > 0) {
+					instance.trigger('error', [ errors ]);
+				}
+
+
+				if (Object.keys(ready).length > 0) {
+					instance.trigger('ready', [ ready, map ]);
+				}
+
+
+				// Reset the clock if the lychee.Preloader timed out
+				if (timedOut === true) {
+					timedOutInstances++;
+				}
+
+			}
+
+		}
+
+
+		if (timedOutInstances === _instances.length) {
+
+			if (lychee.debug === true) {
+				console.log('lychee.Preloader: Nothing to do, switching to idle mode.');
+			}
+
+			for (var i = 0, l = _instances.length; i < l; i++) {
+				_instances[i].__clock = null;
+			}
+
+			global.clearInterval(_globalIntervalId);
+			_globalIntervalId = null;
+
+		}
+
+	};
+
+
+	var Class = function(data) {
+
+		var settings = lychee.extend({}, data);
+
+		settings.timeout  = typeof settings.timeout === 'number' ? settings.timeout : this.defaults.timeout;
+
+
+		this.__timeout  = settings.timeout;
+
+		this.__events  = {};
+		this.__fired   = {}; // cached fired events per request
+		this.__map     = {}; // associated data per request
+		this.__pending = {}; // pending requests
+		this.__clock   = null;
+
+
+		_instances.push(this);
+
+
+		settings = null;
 
 	};
 
@@ -21,9 +123,18 @@
 	Class.prototype = {
 
 		defaults: {
-			interval:  100,
-			timeout:  5000
+			timeout: 3000
 		},
+
+
+
+		/*
+		 * EVENT BINDINGS
+		 *
+		 * (not using lychee.Events
+		 *  due to no-dependency
+		 *  reasons)
+		 */
 
 		bind: function(event, callback, scope) {
 
@@ -50,7 +161,11 @@
 
 			if (event !== null && this.__events[event] !== undefined) {
 				delete this.__events[event];
+				return true;
 			}
+
+
+			return false;
 
 		},
 
@@ -61,51 +176,85 @@
 
 			if (this.__events[event] !== undefined) {
 				this.__events[event].callback.apply(this.__events[event].scope, args);
+				return true;
 			}
+
+
+			return false;
 
 		},
 
-		load: function(urls, data, forced) {
 
-			data = data !== undefined ? data : null;
+
+		/*
+		 * PUBLIC API
+		 */
+
+		load: function(urls, map, forced) {
+
+			urls = typeof urls === 'string' ? [ urls ] : urls;
+			map = map !== undefined ? map : null;
 			forced = typeof forced === 'string' ? forced : null;
 
 
-			this.__lastLoadStart = Date.now();
-
 			if (Object.prototype.toString.call(urls) !== '[object Array]') {
-				urls = [ urls ];
-			}
-
-			if (_globalIntervalId === null) {
-
-				var that = this;
-				_globalIntervalId = global.setInterval(function() {
-					that.__loop();
-				}, this.settings.interval);
-
+				return false;
 			}
 
 
+			this.__clock = Date.now();
+
+
+			// 1. Load the assets via platform-specific APIs
 			for (var u = 0, l = urls.length; u < l; u++) {
 
 				var url = urls[u];
 				var tmp = url.split(/\./);
-				var ext = tmp[tmp.length - 1];
 
 
-				_data[url] = data;
+				if (this.__pending[url] === undefined) {
 
-				this.__load(url, forced || ext);
+					if (map !== null) {
+						this.__map[url] = map;
+					}
 
+
+					// 1.1 Check if another lychee.Preloader
+					// instance already loaded the requested
+					// URL to the shared cache.
+
+					if (_cache[url] != null) {
+
+						this.__pending[url] = false;
+
+					} else {
+
+						if (forced !== null) {
+							this._load(url, forced);
+						} else {
+							this._load(url, tmp[tmp.length - 1]);
+						}
+
+					}
+
+				}
+
+			}
+
+
+			// 2. Start the global interval
+			if (_globalIntervalId === null) {
+				_globalIntervalId = global.setInterval(function() {
+					_globalInterval();
+				}, 100);
 			}
 
 		},
 
 		get: function(url) {
 
-			if (_assets[url] !== undefined) {
-				return _assets[url];
+			if (_cache.resource[url] !== undefined) {
+				return _cache.resource[url];
 			}
 
 
@@ -113,151 +262,41 @@
 
 		},
 
-		__loop: function() {
-
-			var isReady = true;
-
-			var url;
-			for (url in _loading) {
-				if (_assets[url] === undefined) {
-					isReady = false;
-				}
-			}
 
 
-			var timedOut = false;
-			if (this.__lastLoadStart !== null) {
-				timedOut = Date.now() >= this.__lastLoadStart + this.settings.timeout;
-			}
+		/*
+		 * PLATFORM-SPECIFIC Implementation
+		 */
 
-
-			var filtered = {};
-			var data = {};
-			var errors = {};
-			if (
-				isReady === true
-				|| (timedOut && _globalIntervalId !== null)
-			) {
-
-				global.clearInterval(_globalIntervalId);
-				_globalIntervalId = null;
-
-
-
-				// 1. Handle Loading Failures
-				if (timedOut) {
-
-					for (var id in _loading) {
-						if (timedOut && _loading[id] === true && _alreadyFired[id] !== true) {
-							_alreadyFired[id] = true;
-							errors[id] = _data[id] || null;
-						}
-					}
-
-				}
-
-
-				// 2. Handle (parsed) Assets
-				for (var id in _assets) {
-					if (_alreadyFired[id] !== true) {
-
-						_alreadyFired[id] = true;
-
-						// 2.1. Handle incorrect MIME types
-						if (_assets[id] === null) {
-
-							errors[id] = _data[id] || null;
-
-
-						// 2.2. Handle correct MIME types
-						} else {
-
-							filtered[id] = _assets[id];
-							data[id] = _data[id] || null;
-
-						}
-
-					}
-				}
-
-				if (Object.keys(errors).length > 0) {
-					this.trigger('error', [ errors ]);
-				}
-
-
-				this.trigger('ready', [ filtered, data ]);
-
-			}
-
-		},
-
-		__load: function(url, type) {
+		_load: function(url, type) {
 
 			var that = this;
 
-			if (type.match(/jpg|jpeg|png|bmp|gif/)) {
 
-				_loading[url] = true;
+			// 1. JavaScript
+			if (type === 'js') {
 
-				var img = new Image();
-				img.onload = function() {
-					_loading[url] = false;
-					_assets[url] = this;
-				};
-				img.src = url;
-
-			} else if (type === 'css') {
-
-				// CSS won't affect JavaScript stuff
-
-				_loading[url] = false;
-				_assets[url] = '';
-
-				var link = document.createElement('link');
-				link.rel = 'stylesheet';
-				link.href = url;
-
-				document.head.appendChild(link);
-
-			} else if (type === 'js') {
-
-				_loading[url] = true;
+				this.__pending[url] = true;
 
 				var script = document.createElement('script');
 				script.async = true;
-
 				script.onload = function() {
-					_loading[url] = false;
-					_assets[url] = '';
+					that.__pending[url] = false;
+					_cache[url] = '';
 				};
-
-
-				/*
-				var xhr = new XMLHttpRequest();
-				xhr.open('GET', url, true);
-				xhr.onreadystatechange = function() {
-
-					if (xhr.readyState === 4 && xhr.status === 200 || xhr.status === 304) {
-						var data = xhr.responseText || xhr.responseText || null;
-						_assets[url] = data;
-					}
-
-				};
-
-				xhr.send(null);
-				*/
-
 				script.src = url;
+
 				document.body.appendChild(script);
 
+
+			// 2. JSON
 			} else if (type === 'json') {
 
-				_loading[url] = true;
+				this.__pending[url] = true;
 
 				var xhr = new XMLHttpRequest();
 				xhr.open('GET', url, true);
 				xhr.setRequestHeader('Content-Type', 'application/json; charset=utf8');
-
 				xhr.onreadystatechange = function() {
 
 					if (xhr.readyState === 4) {
@@ -269,10 +308,8 @@
 							console.warn('JSON file at ' + url + ' is invalid.');
 						}
 
-
-						// Null will throw MIME errors
-						_assets[url] = data;
-						_loading[url] = false;
+						that.__pending[url] = false;
+						_cache[url] = data;
 
 					}
 
@@ -280,25 +317,48 @@
 
 				xhr.send(null);
 
+
+			// 3. Images
+			} else if (type.match(/bmp|gif|jpg|jpeg|png/)) {
+
+				this.__pending[url] = true;
+
+				var img = new Image();
+				img.onload = function() {
+					that.__pending[url] = false;
+					_cache[url] = this;
+				};
+				img.src = url;
+
+
+			// 4. CSS (won't affect JavaScript anyhow)
+			} else if (type === 'css') {
+
+				this.__pending[url] = false;
+				_cache[url] = '';
+
+				var link = document.createElement('link');
+				link.rel = 'stylesheet';
+				link.href = url;
+
+				document.head.appendChild(link);
+
+
+			// 5. Unknown File Types (will be load as text)
 			} else {
 
-				_loading[url] = true;
+				this.__pending[url] = true;
 
 				var xhr = new XMLHttpRequest();
 				xhr.open('GET', url, true);
-
 				xhr.onreadystatechange = function() {
 
 					if (xhr.readyState === 4 && xhr.status === 200 || xhr.status === 304) {
 
-						var data = xhr.responseText || xhr.responseText || null;
+						var data = xhr.responseText || xhr.responseXML || null;
 
-						if (data !== null) {
-							_loading[url] = false;
-							_assets[url] = data;
-						} else {
-							_loading[url] = false;
-						}
+						that.__pending[url] = false;
+						_cache[url] = data;
 
 					}
 
