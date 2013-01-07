25cc6a86ae940247737ea8a8b0840df480352ba4
Christoph Martens <christoph@martens.ms>
2013-01-07 02:09:26 +0100

    lychee.Viewport (v8gl platform): Initial commit

---
+++ lychee/platform/v8gl/Viewport.js
@@ -0,0 +1,139 @@
+
+lychee.define('Viewport').tags({
+	platform: 'v8gl'
+}).includes([
+	'lychee.Events'
+]).supports(function(lychee, global) {
+
+	if (global.glut) {
+		return true;
+	}
+
+	return false;
+
+}).exports(function(lychee, global) {
+
+	var _instances = [];
+
+	var _listeners = {
+
+		reshape: function(width, height) {
+
+			for (var i = 0, l = _instances.length; i < l; i++) {
+				_instances[i].__processReshape(width, height);
+			}
+
+		},
+
+		visibility: function(state) {
+
+			if (state === glut.VISIBLE) {
+
+				for (var i = 0, l = _instances.length; i < l; i++) {
+					_instances[i].__processShow();
+				}
+
+			} else if (state === glut.NOT_VISIBLE) {
+
+				for (var i = 0, l = _instances.length; i < l; i++) {
+					_instances[i].__processShow();
+				}
+
+			}
+
+		}
+
+	};
+
+
+	(function() {
+
+		var reshape = 'reshapeFunc' in global.glut;
+		if (reshape === true) {
+			glut.reshapeFunc(_listeners.reshape);
+		}
+
+		var visibility = 'visibilityFunc' in global.glut;
+		if (visibility === true) {
+			glut.visibilityFunc(_listeners.visibility);
+		}
+
+
+		if (lychee.debug === true) {
+
+			var methods = [];
+			if (reshape)    methods.push('reshapeFunc');
+			if (visibility) methods.push('visibilityFunc');
+
+			if (methods.length === 0) methods.push('NONE');
+
+			console.log('lychee.Viewport: Supported methods are ' + methods.join(', '));
+
+		}
+
+	})();
+
+
+	var Class = function(data) {
+
+		this.__orientation = null; // Unsupported by freeglut
+		this.__width = 0;
+		this.__height = 0;
+
+
+		lychee.Events.call(this, 'viewport');
+
+		_instances.push(this);
+
+	};
+
+
+	Class.prototype = {
+
+		/*
+		 * PRIVATE API
+		 */
+
+		__processReshape: function(width, height) {
+
+			this.__width  = width;
+			this.__height = height;
+
+
+			if (width > height) {
+
+				this.trigger('reshape', [
+					'landscape',
+					'landscape',
+					width,
+					height
+				]);
+
+			} else {
+
+				this.trigger('reshape', [
+					'portrait',
+					'portrait',
+					width,
+					height
+				]);
+
+			}
+
+		},
+
+		__processShow: function() {
+			this.trigger('show', []);
+		},
+
+		__processHide: function() {
+			this.trigger('hide', []);
+		}
+
+	};
+
+
+	return Class;
+
+});
+
